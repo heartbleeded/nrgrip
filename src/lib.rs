b2df80c3c5e9cb7af39b4981214706c4253d41cd
Matteo Cypriani <mcy@lm7.fr>
2018-05-04 17:22:16 +0200

    lib: enable compile-time warnings

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,17 @@
 
 //! Extracts audio data and metadata from an NRG image of an audio CD.
 
+#![warn(box_pointers,
+        missing_copy_implementations,
+        missing_debug_implementations,
+        //missing_docs,
+        trivial_casts,
+        trivial_numeric_casts,
+        unused_extern_crates,
+        unused_import_braces,
+        unused_qualifications,
+        variant_size_differences)]
+
 pub mod error;
 pub mod metadata;
 pub mod cue_sheet;
